cc5c878291022f32c5efd91fbc02d5cfb6eb1899
31322181 <31322181@RAJLI10223.una.edu.br>
2016-11-03 20:24:25 -0200

    Terminando método para ordenação e pesquisa na memória

--- src/metordenacaopesquisa/MetOrdenacaoPesquisa.java
+++ src/metordenacaopesquisa/MetOrdenacaoPesquisa.java
@@ -1,28 +1,29 @@
-
 package metordenacaopesquisa;
 
 import Pessoas.Aluno;
 
-
 public class MetOrdenacaoPesquisa {
-
 
     public static void main(String[] args) {
 
         ListaAluno lista = new ListaAluno();
-        
-              
+
         lista.inserirAluno(10, "AAAA");
         lista.inserirAluno(9, "BBBB");
         lista.inserirAluno(2, "CCCC");
         lista.inserirAluno(15, "DDDD");
         lista.inserirAluno(20, "EEEE");
-        
-              
-               
-        lista.ordenarAluno();
-        
+
+        lista.inserirAluno(3132, "Thiago Cézar");
+        lista.inserirAluno(3150, "Marcos Sousa");
+
         lista.imprimir();
+        lista.localizarAluno(3132);
+        lista.ordenarAluno();
+
+        lista.imprimir();
+
+        lista.localizarAluno(3132);
     }
-    
+
 }
